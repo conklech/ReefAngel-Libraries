cbf4f61924f5be7c231939b269ad01201448e064
Christian Conkle <christian@conkle.org>
2018-11-20 13:40:23 -0800

    Fix compiler warnings in RA_TouchMenu, including at least one logic bug

--- ReefAngel/RA_TouchMenu.h
+++ ReefAngel/RA_TouchMenu.h
@@ -101,7 +101,7 @@ class SliderClass
 		void DrawMarker();
 		void Show();
 		void Hide();
-		boolean Refresh();
+		void Refresh();
 		boolean IsPlusPressed();
 		boolean IsMinusPressed();
 	private:
